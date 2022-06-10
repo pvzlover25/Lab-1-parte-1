01143d2c641a0553404e6f5e31182fdbbd5b4031
pvzlover25 <105994379+pvzlover25@users.noreply.github.com>
2022-06-09 22:41:34 -0400

    Create Servidor.java

---
+++ Envio sin Cifrar/Servidor/Servidor.java
@@ -0,0 +1,9 @@
+package Servidor;
+import java.awt.*;
+import javax.swing.*;
+
+public class Servidor {
+    public static void main(String[] args){
+        JFrame jf=new VentanaInicialS();
+    }
+}
