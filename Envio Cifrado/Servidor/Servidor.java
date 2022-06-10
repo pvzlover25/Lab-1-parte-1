7d47960d44be24517b3de474d4440b51a2973401
pvzlover25 <105994379+pvzlover25@users.noreply.github.com>
2022-06-09 22:32:31 -0400

    Add files via upload

---
+++ Envio Cifrado/Servidor/Servidor.java
@@ -0,0 +1,10 @@
+package envioarchivo;
+import Servidor.VentanaInicialS;
+import java.awt.*;
+import javax.swing.*;
+
+public class Servidor {
+    public static void main(String[] args){
+        JFrame jf=new VentanaInicialS();
+    }
+}
